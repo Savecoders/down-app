b020eff29a437080f891f87f57026dc168686328
Savecoders <pablopincay2003@gmail.com>
2025-02-20 21:14:33 -0500

    feat: ui style form videl url

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
-import { View } from 'react-native';
-import Animated, { FadeInUp, FadeOutDown, LayoutAnimationConfig } from 'react-native-reanimated';
-import { Info } from '~/lib/icons/Info';
-import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
+import { Image, Text, TextInput, View, ScrollView } from 'react-native';
 import { Button } from '~/components/ui/button';
+import { Input } from '~/components/ui/input';
+import { Label } from '~/components/ui/label';
+import { Download } from '../lib/icons/Download';
+import { Search } from '../lib/icons/Search';
+import { Separator } from '~/components/ui/separator';
+import { H3, H4, P } from '~/components/ui/typography';
 import {
   Card,
   CardContent,
@@ -12,84 +15,60 @@ import {
   CardHeader,
   CardTitle,
 } from '~/components/ui/card';
-import { Progress } from '~/components/ui/progress';
-import { Text } from '~/components/ui/text';
-import { Tooltip, TooltipContent, TooltipTrigger } from '~/components/ui/tooltip';
+import GridVideo from '~/components/GridVideo';
 
-const GITHUB_AVATAR_URI =
-  'https://i.pinimg.com/originals/ef/a2/8d/efa28d18a04e7fa40ed49eeb0ab660db.jpg';
+interface VideoMetadata {
+  title: string;
+  thumbnail: string;
+  duration: string;
+}
 
 export default function Screen() {
-  const [progress, setProgress] = React.useState(78);
+  const inputRef = React.useRef<TextInput>(null);
+  const [value, setValue] = React.useState('');
 
-  function updateProgressValue() {
-    setProgress(Math.floor(Math.random() * 100));
-  }
+  const handleDownload = () => {
+    const url = value;
+    if (!url) {
+      inputRef.current?.focus();
+      return;
+    }
+    // validate url yt or instagram
+    const validatesUrls = ['youtube.com', 'youtu.be'];
+    const isValid = validatesUrls.some(v => url.includes(v));
+    if (!isValid) {
+      console.log('Invalid url');
+      return;
+    }
+    console.log('Downloading...', url);
+  };
+
+  const onChangedText = (text: string) => {
+    setValue(text);
+  };
+
   return (
-    <View className='flex-1 justify-center items-center gap-5 p-6 bg-secondary/30'>
-      <Card className='w-full max-w-sm p-6 rounded-2xl'>
-        <CardHeader className='items-center'>
-          <Avatar alt="Rick Sanchez's Avatar" className='w-24 h-24'>
-            <AvatarImage source={{ uri: GITHUB_AVATAR_URI }} />
-            <AvatarFallback>
-              <Text>RS</Text>
-            </AvatarFallback>
-          </Avatar>
-          <View className='p-3' />
-          <CardTitle className='pb-2 text-center'>Rick Sanchez</CardTitle>
-          <View className='flex-row'>
-            <CardDescription className='text-base font-semibold'>Scientist</CardDescription>
-            <Tooltip delayDuration={150}>
-              <TooltipTrigger className='px-2 pb-0.5 active:opacity-50'>
-                <Info size={14} strokeWidth={2.5} className='w-4 h-4 text-foreground/70' />
-              </TooltipTrigger>
-              <TooltipContent className='py-2 px-4 shadow'>
-                <Text className='native:text-lg'>Freelance</Text>
-              </TooltipContent>
-            </Tooltip>
-          </View>
-        </CardHeader>
-        <CardContent>
-          <View className='flex-row justify-around gap-3'>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Dimension</Text>
-              <Text className='text-xl font-semibold'>C-137</Text>
-            </View>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Age</Text>
-              <Text className='text-xl font-semibold'>70</Text>
-            </View>
-            <View className='items-center'>
-              <Text className='text-sm text-muted-foreground'>Species</Text>
-              <Text className='text-xl font-semibold'>Humano</Text>
-            </View>
-          </View>
-        </CardContent>
-        <CardFooter className='flex-col gap-3 pb-0'>
-          <View className='flex-row items-center overflow-hidden'>
-            <Text className='text-sm text-muted-foreground'>Productivitys</Text>
-            <LayoutAnimationConfig skipEntering>
-              <Animated.View
-                key={progress}
-                entering={FadeInUp}
-                exiting={FadeOutDown}
-                className='w-11 items-center'
-              >
-                <Text className='text-sm font-bold text-sky-600'>{progress}%</Text>
-              </Animated.View>
-            </LayoutAnimationConfig>
-          </View>
-          <Progress value={progress} className='h-2' indicatorClassName='bg-sky-600' />
-          <View />
-          <Button
-            variant='outline'
-            className='shadow shadow-foreground/5'
-            onPress={updateProgressValue}
-          >
-            <Text>Update</Text>
+    <ScrollView className='flex-1'>
+      <View className='flex-1 gap-6 p-6 w-full h-full'>
+        <Label className='native:text-2xl'>Download Url Video</Label>
+        <View className='flex flex-row gap-6'>
+          <Input
+            className='flex-1'
+            placeholder='https://'
+            ref={inputRef}
+            onChangeText={onChangedText}
+          />
+          <Button className='w-14 native:h-14' variant='default' onPress={handleDownload}>
+            <Search className='native:w-6 native:h-6 dark:text-zinc-900 text-white' />
           </Button>
-        </CardFooter>
-      </Card>
-    </View>
+        </View>
+        <View className='relative flex items-center'>
+          <Separator decorative={true} className='absolute w-full top-1/2' />
+          <P className='px-4 bg-background relative z-10 text-zinc-400'>Videos Result (1)</P>
+        </View>
+
+        <GridVideo />
+      </View>
+    </ScrollView>
   );
 }
