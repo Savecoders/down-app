b30efa7ec2ba395bbf12b64578d644da0c2ef6c1
Savecoders <pablopincay2003@gmail.com>
2025-02-21 21:37:22 -0500

    chore: vault export icons

---
+++ lib/icons/index.ts
@@ -0,0 +1,8 @@
+export { Info } from './Info';
+export { MoonStar } from './MoonStar';
+export { Download } from './Download';
+export { Sun } from './Sun';
+export { Search } from './Search';
+export { RefreshCw } from './RefreshCw';
+export { X } from './X';
+export { LoaderCircle } from './LoaderCircle';
